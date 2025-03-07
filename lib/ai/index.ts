6c00637df07776ce37e362a02e717970d5638b3f
Flory <flory.contato@gmail.com>
2025-03-07 18:51:58 -0300

    Add google gemini and db

--- lib/ai/index.ts
+++ lib/ai/index.ts
@@ -1,11 +1,32 @@
-import { openai } from '@ai-sdk/openai';
-import { experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
+import { google } from "@ai-sdk/google";
+import { experimental_wrapLanguageModel as wrapLanguageModel } from "ai";
 
-import { customMiddleware } from './custom-middleware';
+import { customMiddleware } from "./custom-middleware";
 
-export const customModel = (apiIdentifier: string) => {
+export type GoogleGenerativeAIModelId =
+  | "gemini-2.0-flash-001"
+  | "gemini-1.5-flash"
+  | "gemini-1.5-flash-latest"
+  | "gemini-1.5-flash-001"
+  | "gemini-1.5-flash-002"
+  | "gemini-1.5-flash-8b"
+  | "gemini-1.5-flash-8b-latest"
+  | "gemini-1.5-flash-8b-001"
+  | "gemini-1.5-pro"
+  | "gemini-1.5-pro-latest"
+  | "gemini-1.5-pro-001"
+  | "gemini-1.5-pro-002"
+  | "gemini-2.0-flash-lite-preview-02-05"
+  | "gemini-2.0-pro-exp-02-05"
+  | "gemini-2.0-flash-thinking-exp-01-21"
+  | "gemini-2.0-flash-exp"
+  | "gemini-exp-1206"
+  | "learnlm-1.5-pro-experimental"
+  | (string & {});
+
+export const customModel = (apiIdentifier: GoogleGenerativeAIModelId) => {
   return wrapLanguageModel({
-    model: openai(apiIdentifier),
+    model: google("gemini-2.0-flash-001") as any,
     middleware: customMiddleware,
   });
 };
--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -1,25 +1,28 @@
 // Define your models here.
 
+import { GoogleGenerativeAIModelId } from ".";
+
 export interface Model {
-  id: string;
-  label: string;
-  apiIdentifier: string;
+  id: GoogleGenerativeAIModelId;
+  label: GoogleGenerativeAIModelId;
+  apiIdentifier: GoogleGenerativeAIModelId;
   description: string;
 }
 
 export const models: Array<Model> = [
   {
-    id: 'gpt-4o-mini',
-    label: 'GPT 4o mini',
-    apiIdentifier: 'gpt-4o-mini',
-    description: 'Small model for fast, lightweight tasks',
+    id: "gemini-2.0-flash-001",
+    label: "gemini-2.0-flash-001",
+    apiIdentifier: "",
+    description: "Small model for fast, lightweight tasks",
   },
   {
-    id: 'gpt-4o',
-    label: 'GPT 4o',
-    apiIdentifier: 'gpt-4o',
-    description: 'For complex, multi-step tasks',
+    id: "gemini-2.0-flash-thinking-exp-01-21",
+    label: "gemini-2.0-flash-thinking-exp-01-21",
+    apiIdentifier: "gemini-2.0-flash-thinking-exp-01-21",
+    description: "For complex, multi-step tasks",
   },
 ] as const;
 
-export const DEFAULT_MODEL_NAME: string = 'gpt-4o-mini';
+export const DEFAULT_MODEL_NAME: GoogleGenerativeAIModelId =
+  "gemini-2.0-flash-001";
